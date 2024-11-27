51b4cb11c612721784b3a3545a216594adcbec2b
Linkan333 <linunybe@gmail.com>
2024-11-27 13:18:30 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Navbar.css';
 
-function App() {
+function Navbar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={`${process.env.PUBLIC_URL}/BlockLancer.png`} alt="BlockLancer Logo" />
+        Freelance Marketplace
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#post-job">Post Job</a></li>
+        <li><a href="#jobs">Jobs</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
   );
 }
 
-export default App;
+export default Navbar;
